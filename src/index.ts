95fc4b15bee7375c9f72f4ce1daa15f7fdcca3a9
Filipp Goriainov <barrios1991phil@gmail.com>
2024-12-18 22:58:34 +0400

    add reverse pitch map

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,3 @@
 export * from "./types/notes";
-export { pitchMap } from "./utils/pitchMap";
+export { pitchMap, pitchMapReverse } from "./utils/pitchMap";
 export { produceMelody } from "./sounds-generators/produceMelody";
